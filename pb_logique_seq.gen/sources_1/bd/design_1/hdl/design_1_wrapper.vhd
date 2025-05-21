ff7c9b5d2d67bb0a0758878030d3de2733ee4b90
Benjamin Chausse <benjamin@chausse.xyz>
2025-05-20 21:06:01 -0400

    asdf

--- pb_logique_seq.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ pb_logique_seq.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
---Date        : Mon May 19 17:29:36 2025
---Host        : DESKTOP-GPNP2A0 running 64-bit major release  (build 9200)
+--Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
+--Date        : Tue May 20 18:30:29 2025
+--Host        : workstation running 64-bit Artix Linux
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
 --Purpose     : IP block netlist
